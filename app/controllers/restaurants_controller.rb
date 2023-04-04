850faf1e71ebcc9488ef5274f43d41c2e9f45fcf
gaston henrich <henrichgaston@gmail.com>
2023-04-04 10:19:26 -0300

    cambios en el controller y edit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,4 +13,19 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user_id = @restaurant.current_user.id
+    if @restaurant.save
+      redirect_to @restaurant, notice: "El restaurante se ha creado con éxito"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :specialty, :description, :image)
+  end
 end
